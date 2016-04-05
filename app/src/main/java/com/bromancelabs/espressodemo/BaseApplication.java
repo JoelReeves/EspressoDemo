e71e6f3405fda01febe2736c8af389390b79e700
Joel Reeves <jreeves@tonicdesign.com>
2016-04-05 14:05:53 -0400

    adding Timber for logging

--- app/src/main/java/com/bromancelabs/espressodemo/BaseApplication.java
+++ app/src/main/java/com/bromancelabs/espressodemo/BaseApplication.java
@@ -2,10 +2,16 @@ package com.bromancelabs.espressodemo;
 
 import android.app.Application;
 
+import timber.log.Timber;
+
 public class BaseApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
     }
 }
