ee57f4fe2e1ddadca752033b6dd7d6c168ce9485
Joel Reeves <jreeves@tonicdesign.com>
2016-04-05 14:04:26 -0400

    initial project setup

---
+++ app/src/main/java/com/bromancelabs/espressodemo/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.bromancelabs.espressodemo.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bromancelabs.espressodemo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
