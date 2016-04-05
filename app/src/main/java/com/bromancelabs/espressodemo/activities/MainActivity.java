64902a535507b75f775b54ae3e7cec5b39e8ed34
Joel Reeves <jreeves@tonicdesign.com>
2016-04-05 14:23:12 -0400

    wiring up EditText field and button click listener

--- app/src/main/java/com/bromancelabs/espressodemo/activities/MainActivity.java
+++ app/src/main/java/com/bromancelabs/espressodemo/activities/MainActivity.java
@@ -2,16 +2,33 @@ package com.bromancelabs.espressodemo.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.widget.EditText;
 
 import com.bromancelabs.espressodemo.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.txt_inputField) EditText inputEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ButterKnife.bind(this);
+    }
 
+    @OnClick(R.id.btn_submit)
+    public void submitButtonClicked() {
+        String text = inputEditText.getText().toString();
+
+        if (!TextUtils.isEmpty(text)) {
+            
+        }
     }
 }
