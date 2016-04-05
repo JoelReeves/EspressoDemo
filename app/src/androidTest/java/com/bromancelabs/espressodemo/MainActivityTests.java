949788e704459783d8487ea7b489e88e67192c66
Joel Reeves <jreeves@tonicdesign.com>
2016-04-05 16:22:42 -0400

    adding text to test for EditText input

--- app/src/androidTest/java/com/bromancelabs/espressodemo/MainActivityTests.java
+++ app/src/androidTest/java/com/bromancelabs/espressodemo/MainActivityTests.java
@@ -11,6 +11,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -18,6 +20,8 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTests {
+
+    private static final String INPUT_TEXT = "Espresso";
 
     @Rule
     public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
@@ -31,5 +35,11 @@ public class MainActivityTests {
     @Test
     public void shouldHaveSubmitButton() {
         onView(withId(R.id.btn_submit)).check(ViewAssertions.matches(isDisplayed()));
+    }
+
+    @Test
+    public void editTextShouldContainText() {
+        onView(withId(R.id.txt_inputField)).perform(typeText(INPUT_TEXT), closeSoftKeyboard());
+        onView(withId(R.id.txt_inputField)).check(matches(withText(INPUT_TEXT)));
     }
 }
