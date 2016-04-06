977b11d3b63b938ba9a1b2212656f51888a290c4
Joel Reeves <jreeves@tonicdesign.com>
2016-04-06 17:12:54 -0400

    test to confirm recyclerview has Batman in the list

--- app/src/androidTest/java/com/bromancelabs/espressodemo/SuperHeroActivityTests.java
+++ app/src/androidTest/java/com/bromancelabs/espressodemo/SuperHeroActivityTests.java
@@ -9,10 +9,14 @@ import org.junit.Test;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 public class SuperHeroActivityTests {
+
+    private static final String BATMAN = "Batman";
 
     @Rule
     public ActivityTestRule<SuperHeroActivity> activityRule = new ActivityTestRule<>(SuperHeroActivity.class);
@@ -20,5 +24,10 @@ public class SuperHeroActivityTests {
     @Test
     public void recyclerViewShouldBeVisible() {
         onView(withId(R.id.recyclerView)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void recyclerViewShouldHaveBatmanHero() {
+        onView(withId(R.id.recyclerView)).check(matches(hasDescendant(withText(BATMAN))));
     }
 }
