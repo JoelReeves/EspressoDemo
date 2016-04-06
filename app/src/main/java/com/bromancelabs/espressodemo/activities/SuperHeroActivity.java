c8225f0ffdbe6d797e59fb309ee8ded3f14b2d1c
Joel Reeves <jreeves@tonicdesign.com>
2016-04-06 16:36:15 -0400

    populating items in the RecyclerView

--- app/src/main/java/com/bromancelabs/espressodemo/activities/SuperHeroActivity.java
+++ app/src/main/java/com/bromancelabs/espressodemo/activities/SuperHeroActivity.java
@@ -4,14 +4,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.bromancelabs.espressodemo.R;
+import com.bromancelabs.espressodemo.models.SuperHero;
+import com.bromancelabs.espressodemo.views.HeroAdapter;
+import com.bromancelabs.espressodemo.views.SimpleDividerItemDecoration;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import timber.log.Timber;
 
 public class SuperHeroActivity extends AppCompatActivity {
 
@@ -23,6 +31,8 @@ public class SuperHeroActivity extends AppCompatActivity {
         setContentView(R.layout.activity_super_hero);
 
         ButterKnife.bind(this);
+
+        setupRecyclerView();
     }
 
     @Override
@@ -40,5 +50,24 @@ public class SuperHeroActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    private void setupRecyclerView() {
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        String[] heroStringArray = getResources().getStringArray(R.array.heroes);
+        List<SuperHero> heroList = new ArrayList<>();
+
+        for (String h: heroStringArray) {
+            heroList.add(new SuperHero(h));
+        }
+
+        Timber.d(heroList.toString());
+
+        HeroAdapter heroAdapter = new HeroAdapter(heroList);
+
+        recyclerView.addItemDecoration(new SimpleDividerItemDecoration(this, R.drawable.recyclerview_horizontal_divider));
+        recyclerView.setAdapter(heroAdapter);
     }
 }
