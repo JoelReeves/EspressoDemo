ca8140e135fca349cbe72209e43088576281225f
Joel Reeves <jreeves@tonicdesign.com>
2016-04-06 14:47:32 -0400

    wrote test to check that the intent has an extra

--- app/src/androidTest/java/com/bromancelabs/espressodemo/IntentTests.java
+++ app/src/androidTest/java/com/bromancelabs/espressodemo/IntentTests.java
@@ -1,4 +1,35 @@
 package com.bromancelabs.espressodemo;
 
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+
+import com.bromancelabs.espressodemo.activities.MainActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtra;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.toPackage;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
 public class IntentTests {
+
+    private static final String INPUT_TEXT = "Adrienne hates...";
+    private static final String ACTIVITY_PACKAGE_NAME = "com.bromancelabs.espressodemo";
+    private static final String EXTRA_TEXT = "extra_text";
+
+    @Rule
+    public IntentsTestRule<MainActivity> intentRule = new IntentsTestRule<>(MainActivity.class);
+
+    @Test
+    public void intentShouldHaveExtra() {
+        onView(withId(R.id.txt_inputField)).perform(typeText(INPUT_TEXT), closeSoftKeyboard());
+        onView(withId(R.id.btn_submit)).perform(click());
+        intended(toPackage(ACTIVITY_PACKAGE_NAME));
+        intended(hasExtra(EXTRA_TEXT, INPUT_TEXT));
+    }
 }
