6cfed33e2802bad056e4a7f8648e65b70db7b171
Joel Reeves <jreeves@tonicdesign.com>
2016-04-06 15:58:33 -0400

    created model class for heroes

---
+++ app/src/main/java/com/bromancelabs/espressodemo/models/SuperHero.java
@@ -0,0 +1,14 @@
+package com.bromancelabs.espressodemo.models;
+
+public class SuperHero {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
