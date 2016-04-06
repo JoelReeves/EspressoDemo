fc56bf65497d4e9d7ed7fe31fb3e6ba2b209ffed
Joel Reeves <jreeves@tonicdesign.com>
2016-04-06 17:13:18 -0400

    test to confirm that DareDevil is in the 5th position

--- app/src/androidTest/java/com/bromancelabs/espressodemo/SuperHeroActivityTests.java
+++ app/src/androidTest/java/com/bromancelabs/espressodemo/SuperHeroActivityTests.java
@@ -13,10 +13,13 @@ import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static com.bromancelabs.espressodemo.utils.RecyclerViewUtils.atPosition;
 
 public class SuperHeroActivityTests {
 
     private static final String BATMAN = "Batman";
+    private static final String DAREDEVIL = "DareDevil";
+    private static final int DAREDEVIL_POSITION = 4;
 
     @Rule
     public ActivityTestRule<SuperHeroActivity> activityRule = new ActivityTestRule<>(SuperHeroActivity.class);
@@ -29,5 +32,11 @@ public class SuperHeroActivityTests {
     @Test
     public void recyclerViewShouldHaveBatmanHero() {
         onView(withId(R.id.recyclerView)).check(matches(hasDescendant(withText(BATMAN))));
+    }
+
+    @Test
+    public void recyclerViewShouldHaveDaredevilAsFifthItem() {
+        onView(withId(R.id.recyclerView))
+                .check(matches(atPosition(DAREDEVIL_POSITION, hasDescendant(withText(DAREDEVIL)))));
     }
 }
