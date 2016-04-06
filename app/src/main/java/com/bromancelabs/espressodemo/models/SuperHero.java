86cd075949d19a5e06a06e5c089b1d0bffc218fc
Joel Reeves <jreeves@tonicdesign.com>
2016-04-06 16:33:15 -0400

    overriding toString method

--- app/src/main/java/com/bromancelabs/espressodemo/models/SuperHero.java
+++ app/src/main/java/com/bromancelabs/espressodemo/models/SuperHero.java
@@ -1,7 +1,7 @@
 package com.bromancelabs.espressodemo.models;
 
 public class SuperHero {
-    
+
     public SuperHero(String name) {
         this.name = name;
     }
@@ -14,5 +14,10 @@ public class SuperHero {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
     }
 }
