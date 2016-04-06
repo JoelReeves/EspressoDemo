c9f3cf5411986b4345bacb576e9d8dff924a90e0
Joel Reeves <jreeves@tonicdesign.com>
2016-04-06 15:44:29 -0400

    inflating menu to go to the InputDataActivity

--- app/src/main/java/com/bromancelabs/espressodemo/activities/SuperHeroActivity.java
+++ app/src/main/java/com/bromancelabs/espressodemo/activities/SuperHeroActivity.java
@@ -1,9 +1,12 @@
 package com.bromancelabs.espressodemo.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.bromancelabs.espressodemo.R;
 
@@ -20,5 +23,22 @@ public class SuperHeroActivity extends AppCompatActivity {
         setContentView(R.layout.activity_super_hero);
 
         ButterKnife.bind(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_hero, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuStartInputDataActivity:
+                startActivity(new Intent(this, InputDataActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
